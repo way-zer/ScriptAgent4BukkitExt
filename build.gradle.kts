293c54dc1c5c08d06480b6a53080c545640e1530
way-zer <himc.wicp@gmail.com>
2021-02-10 14:56:34 +0800

    [Update]更新项目结构与其他项目同步 [Update](loader) 更新库到v1.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.4.20"
     id("me.qoomon.git-versioning") version "2.1.1"
     id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "cf.wayzer"
 version = "v1.x.x" //采用3位版本号v1.2.3 1为大版本 2为插件版本 3为脚本版本
-val libraryVersion = "1.2.8"
-val mcVersion = "1.14-R0.1-SNAPSHOT"
 
 gitVersioning.apply(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig> {
     tag(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
@@ -15,64 +13,46 @@ gitVersioning.apply(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPlugin
         versionFormat = "\${tagVersion}"
     })
     commit(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.CommitVersionDescription> {
-        versionFormat = "\${version}-\${commit.short}\${dirty}"
+        versionFormat = "\${commit.short}-SNAPSHOT"
     })
 })
 
-repositories {
-    mavenLocal()
-    jcenter()
-    mavenCentral()
-    maven("https://dl.bintray.com/way-zer/maven")
-    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-
-    maven("https://repo.codemc.org/repository/maven-public")//nbt-api
-    maven("https://dl.bintray.com/config4k/config4k")//config4k
-}
-sourceSets{
+sourceSets {
     main {
-        java.srcDir("src")
+        java.srcDir("scripts")
     }
     create("plugin") {
-        this.compileClasspath += main.get().compileClasspath
         java.srcDir("plugin/src")
         resources.srcDir("plugin/res")
     }
 }
-dependencies {
-    api("cf.wayzer:ScriptAgent:$libraryVersion")
-    implementation(kotlin("script-runtime"))
-    implementation(kotlin("stdlib-jdk8"))
-    //也可以直接使用服务器正在使用的jar(使用NMS等)
-    implementation("org.spigotmc:spigot-api:$mcVersion")
 
-    //used by superitem
-    implementation("de.tr7zw:item-nbt-api:2.2.0")
-    implementation("org.mapdb:mapdb:3.0.7")
-
-    //coreLibrary
-    api("cf.wayzer:PlaceHoldLib:2.1.0")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
-    implementation("org.jetbrains.exposed:exposed-core:0.24.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.24.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.24.1")
-    implementation("io.github.config4k:config4k:0.4.1")
+apply {
+    from("dependencies.gradle.kts")
 }
 
 tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
-    withType<ProcessResources>{
+    withType<ProcessResources> {
         inputs.property("version", rootProject.version)
         filter(
                 filterType = org.apache.tools.ant.filters.ReplaceTokens::class,
                 properties = mapOf("tokens" to mapOf("version" to rootProject.version))
         )
     }
-    create<Zip>("scriptsZip"){
+    named<Delete>("clean") {
+        this.delete += fileTree("scripts").filter { it.name.endsWith(".cache.jar") }
+        this.delete += fileTree("scripts/cache")
+    }
+    create<Zip>("scriptsZip") {
         group = "plugin"
-        from(sourceSets.main.get().allSource)
+        from(sourceSets.main.get().allSource) {
+            exclude("*.ktc")
+            exclude("cache.jar")
+            exclude(".metadata")
+        }
         archiveClassifier.set("scripts")
     }
     create<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("buildPlugin") {
@@ -80,9 +60,10 @@ tasks {
         group = "plugin"
         from(sourceSets.getByName("plugin").output)
         archiveClassifier.set("")
-        configurations = listOf(project.configurations.getByName("compileClasspath"))
+        archiveVersion.set(rootProject.version.toString().substringBeforeLast('.'))
+        configurations = listOf(project.configurations.getByName("pluginCompileClasspath"))
         dependencies {
-            include(dependency("cf.wayzer:ScriptAgent:$libraryVersion"))
+            include(dependency("cf.wayzer:ScriptAgent"))
             include(dependency("cf.wayzer:LibraryManager"))
         }
     }
---
+++ dependencies.gradle.kts
@@ -0,0 +1,43 @@
+val libraryVersion = "1.4.2"
+val mcVersion = "1.14-R0.1-SNAPSHOT"
+val exposedVersionn = "0.29.1"
+
+repositories {
+    mavenLocal()
+    jcenter()
+    mavenCentral()
+    maven("https://dl.bintray.com/way-zer/maven")
+    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+
+    maven("https://repo.codemc.org/repository/maven-public")//nbt-api
+    maven("https://dl.bintray.com/config4k/config4k")//config4k
+}
+
+dependencies {
+    val pluginCompile by configurations
+    pluginCompile("cf.wayzer:ScriptAgent:$libraryVersion")
+    pluginCompile("cf.wayzer:LibraryManager:1.4")
+    pluginCompile(kotlin("stdlib-jdk8"))
+    pluginCompile("org.spigotmc:spigot-api:$mcVersion")
+
+    val api by configurations
+    val implementation by configurations
+    api(kotlin("script-runtime"))
+    api(kotlin("stdlib-jdk8"))
+    api("cf.wayzer:ScriptAgent:$libraryVersion")
+
+    //coreLibrary
+    api("cf.wayzer:PlaceHoldLib:3.1")
+    api("org.jetbrains.exposed:exposed-core:$exposedVersionn")
+    api("org.jetbrains.exposed:exposed-dao:$exposedVersionn")
+    api("org.jetbrains.exposed:exposed-java-time:$exposedVersionn")
+    api("io.github.config4k:config4k:0.4.1")
+
+    //coreBukkit
+    //也可以直接使用服务器正在使用的jar(使用NMS等)
+    api("org.spigotmc:spigot-api:$mcVersion")
+
+    //used by superitem
+    api("de.tr7zw:item-nbt-api:2.2.0")
+    api("org.mapdb:mapdb:3.0.7")
+}
